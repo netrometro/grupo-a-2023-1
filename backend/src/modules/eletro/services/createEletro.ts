4eaed08bb7791fa2125f421d93b177a7924015f9
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-08-19 01:02:31 -0300

    fix: putting the same form to both codes

--- backend/src/modules/eletro/services/createEletro.ts
+++ backend/src/modules/eletro/services/createEletro.ts
@@ -3,24 +3,25 @@ import { CreateEletroDTO } from "../dtos/CreateEletroDTO";
 import { prisma } from "../../../prisma/client";
 import { AppError } from "../../../error/AppError";
 
-export class CreateEletro {
-  async execute({ nome, kwh }: CreateEletroDTO): Promise<Eletro> {
-    const eletroAlreadyExist = await prisma.eletro.findUnique({
-      where: {
-        nome,
-      },
-    });
+export const CreateEletro = async ({
+  nome,
+  kwh,
+}: CreateEletroDTO): Promise<Eletro> => {
+  const eletroAlreadyExist = await prisma.eletro.findUnique({
+    where: {
+      nome,
+    },
+  });
 
-    if (eletroAlreadyExist) {
-      throw new AppError("Eletro ja existe");
-    }
-
-    const eletro = await prisma.eletro.create({
-      data: {
-        nome,
-        kwh,
-      },
-    });
-    return eletro;
+  if (eletroAlreadyExist) {
+    throw new AppError("Eletro ja existe");
   }
-}
+
+  const eletro = await prisma.eletro.create({
+    data: {
+      nome,
+      kwh,
+    },
+  });
+  return eletro;
+};
