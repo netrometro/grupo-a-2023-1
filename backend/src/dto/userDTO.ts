f9aa157bc065e8925dafdcccc4c237007e84da10
Ana Laura <analaurass45@gmail.com>
2023-08-19 10:18:44 -0300

    feat: creating the type userDto with zod

---
+++ backend/src/dto/userDTO.ts
@@ -0,0 +1,19 @@
+import {z} from "zod";
+
+const userDTO = z.object({
+    email: z.string({
+        required_error: "email is required",
+        invalid_type_error: "email must be a string"
+    }).email(),
+    name: z.string({
+        required_error: "name is required",
+        invalid_type_error: "name must be a string"
+    }),
+    password: z.string({
+        required_error: "password is required",
+        invalid_type_error: "password must be a string"
+    }),
+});
+
+export type userDTO = z.infer<typeof userDTO>;
+
