3b257bcc302639d5ba98cb730ec2d58e2164c4f1
Jamuelton <78917005+Jamuelton@users.noreply.github.com>
2023-08-19 01:08:08 -0300

    chore: adding a error condicion for not exist id request

--- backend/src/modules/eletro/services/getEletro.ts
+++ backend/src/modules/eletro/services/getEletro.ts
@@ -1,17 +1,23 @@
 import { Eletro } from "@prisma/client";
 import { prisma } from "../../../prisma/client";
 import { CreateEletroDTO } from "../dtos/CreateEletroDTO";
+import { AppError } from "../../../error/AppError";
 
-export const GetEletro = async () => {
+export const GetEletro = async (): Promise<Eletro[]> => {
   const response = await prisma.eletro.findMany();
   return response;
 };
 
-export const GetEletroById = async (id: number) => {
-  const reponse = await prisma.eletro.findUnique({
+export const GetEletroById = async (
+  id: number
+): Promise<CreateEletroDTO | null> => {
+  const response = await prisma.eletro.findUnique({
     where: {
       id,
     },
   });
-  return reponse;
+  if (response == null) {
+    throw new AppError("Eletro ja existe");
+  }
+  return response;
 };
