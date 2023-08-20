26dc9dd840c1a992a1ce7b7c869891e67b2ef69f
rikcav <henriquearau000@gmail.com>
2023-08-20 11:33:56 -0300

    feat: consumoDTO

---
+++ backend/src/dto/consumoDTO.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+import { consumoEletroDTO } from "./consumoEletroDTO";
+
+const consumoDTO = z.object({
+  date: z.date({
+    required_error: "date is required",
+    invalid_type_error: "date must be a date",
+  }),
+  dinheiro: z.number({
+    required_error: "dinheiro is required",
+    invalid_type_error: "dinheiro must be a number",
+  }),
+  kwh: z.number({
+    required_error: "kwh is required",
+    invalid_type_error: "kwh must be a number",
+  }),
+  consumos: z.array(consumoEletroDTO),
+});
+
+export type consumoDTO = z.infer<typeof consumoDTO>;
