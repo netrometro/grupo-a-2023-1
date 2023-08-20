ee3deb96baa2085bbd21b28fa9c01aceec942dcd
rikcav <henriquearau000@gmail.com>
2023-08-20 18:48:30 -0300

    feat: route added

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import Fastify from "fastify";
 import userRoutes from "./routes/userRoutes";
 import consumoEletroRoutes from "./routes/consumoEletroRoutes";
 import consumoRoutes from "./routes/consumoRoutes";
+import eletroRoutes from "./routes/EletroRoutes/EletroRouts";
 import cors from "@fastify/cors";
 
 const server = Fastify();
@@ -15,38 +16,8 @@ server.register(cors, { origin: true });
 async function main() {
   server.register(userRoutes, { prefix: "api/user" });
   server.register(consumoEletroRoutes, { prefix: "api/consumoEletro" });
-  server.register(consumoRoutes, { prefix: "api/consumo"})
-
-  try {
-    await server.listen(3000);
-    console.log("Server ready at port 3000");
-  } catch (e) {
-    console.log(e);
-    process.exit(1);
-  }
-}
-
-main();
-import Fastify from "fastify";
-import userRoutes from "./routes/userRoutes";
-import consumoEletroRoutes from "./routes/consumoEletroRoutes";
-import consumoRoutes from "./routes/consumoRoutes";
-import cors from "@fastify/cors";
-import cors from "@fastify/cors";
-
-const server = Fastify();
-
-server.get("/healthcheck", async function () {
-  return { status: "ok" };
-});
-
-server.register(cors, { origin: true });
-
-async function main() {
-  server.register(cors, { origin: true });
-  server.register(userRoutes, { prefix: "api/user" });
-  server.register(consumoEletroRoutes, { prefix: "api/consumoEletro" });
-  server.register(consumoRoutes, { prefix: "api/consumo"})
+  server.register(consumoRoutes, { prefix: "api/consumo" });
+  server.register(eletroRoutes, { prefix: "api/eletro" });
 
   try {
     await server.listen(3000);
