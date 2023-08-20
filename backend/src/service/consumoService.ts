b7077103364f086b99810b1aaa1cc7ab6c54985b
rikcav <henriquearau000@gmail.com>
2023-08-20 15:54:36 -0300

    feat: updateConsumo

--- backend/src/service/consumoService.ts
+++ backend/src/service/consumoService.ts
@@ -1,12 +1,12 @@
 import { consumoDTO } from "../dto/consumoDTO";
 import prisma from "../prisma/prisma";
 
-// export async function createConsumo(input: consumoDTO) {
-//   const consumo = await prisma.consumo.create({
-//     data: input,
-//   });
-//   return consumo;
-// }
+export async function createConsumo(input: consumoDTO) {
+  const consumo = await prisma.consumo.create({
+    data: input,
+  });
+  return consumo;
+}
 
 export async function listConsumos() {
   const consumos = await prisma.consumo.findMany();
@@ -21,6 +21,33 @@ export async function listConsumo(input: Number) {
   if (!consumo) {
     throw new Error("consumo does not exit");
   }
+
+  return consumo;
+}
+
+export async function updateConsumo(id: Number, body: consumoDTO) {
+  const consumo = await prisma.consumo.findUnique({
+    where: { id: Number(id) },
+  });
+
+  if (!consumo) {
+    throw new Error("consumo not found");
+  }
+
+  const { userId, date, dinheiro, kwh /* consumos */ } = body;
+
+  const update = await prisma.consumo.update({
+    where: {
+      id: Number(id),
+    },
+    data: {
+      date: date,
+      dinheiro: dinheiro,
+      kwh: kwh,
+      userId: userId,
+      //   consumos: consumos,
+    },
+  });
 
   return consumo;
 }
