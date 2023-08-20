2c32c748306e6f504746842b9675bbb9587ff6d5
Ana Laura <analaurass45@gmail.com>
2023-08-20 17:13:56 -0300

    feat: creating button component

---
+++ frontend/selpe-app/src/components/button/style.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#2980B9',
+    width: '90%',
+    alignSelf: 'center',
+    padding: 15,
+    borderRadius: 22
+  },
+  text: {
+    alignSelf: 'center',
+    color: '#FFFFFF'
+  }
+});
