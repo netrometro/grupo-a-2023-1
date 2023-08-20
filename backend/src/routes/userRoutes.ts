eb741827553e05a6fca0b4c4542d1a14947d8c8b
Ana Laura <analaurass45@gmail.com>
2023-08-19 21:48:00 -0300

    feat: adding new routes

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,8 +1,13 @@
 import {FastifyInstance} from "fastify";
-import { registerUser } from "../controller/userController";
+import { findUser, findUsers, loginUser, registerUser, removeUser, upgradeUser } from "../controller/userController";
 
 async function userRoutes(server: FastifyInstance){
+    server.get('/:id', findUser);
+    server.get('/users', findUsers);
+    server.put('/:id', upgradeUser)
     server.post('/', registerUser);
+    server.post('/login', loginUser);
+    server.post('/delete/:id', removeUser);    
 }
 
 export default userRoutes;
