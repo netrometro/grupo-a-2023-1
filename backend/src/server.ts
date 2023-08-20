cd010495c68aac486c9c0b24c4d9357a3ebde850
Ana Laura <analaurass45@gmail.com>
2023-08-19 21:48:23 -0300

    feat: updating server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,25 +1,25 @@
 import Fastify from "fastify";
 import userRoutes from "./routes/userRoutes";
+import cors from "@fastify/cors";
 
 const server = Fastify();
 
-// just to check if its running
 server.get("/healthcheck", async function () {
-    return {status: "ok"};
+  return { status: "ok" };
 });
 
-async function main(){
+server.register(cors, { origin: true });
 
-    server.register(userRoutes, {prefix: 'api/user'})
+async function main() {
+  server.register(userRoutes, { prefix: "api/user" });
 
-    try{
-        await server.listen(3000);
-        console.log("Server ready at port 3000");
-    }
-    catch(e){
-        console.log(e);
-        process.exit(1);
-    }
+  try {
+    await server.listen(3000);
+    console.log("Server ready at port 3000");
+  } catch (e) {
+    console.log(e);
+    process.exit(1);
+  }
 }
 
 main();
