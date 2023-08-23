7e327d3ccf0cf87ed3b331f8bef8d62e5beb22a7
rikcav <henriquearau000@gmail.com>
2023-08-23 14:07:43 -0300

    fix: alterações no componente

--- frontend/selpe-app/src/components/consumolist/ConsumoList.tsx
+++ frontend/selpe-app/src/components/consumolist/ConsumoList.tsx
@@ -8,23 +8,19 @@ interface ConsumoListProps {
   date: Date;
   kwh: number;
   dinheiro: number;
-  //   editFunc(): void;
-  //   deleteFunc(): void;
+  editFunc(): void;
+  deleteFunc(): void;
 }
 
-export const ConsumoList = ({
-  date,
-  kwh,
-  dinheiro /* editFunc, deleteFunc */
-}: ConsumoListProps) => {
+export const ConsumoList = ({ date, kwh, dinheiro, editFunc, deleteFunc }: ConsumoListProps) => {
   return (
     <View style={listStyle.container}>
       <Text>{String(date)}</Text>
       <Text>{kwh}</Text>
       <Text>{dinheiro}</Text>
       <View style={listStyle.iconsView}>
-        {/* <FontAwesome5 name="pen" size={20} color="#2980B9" onPress={editFunc} />
-        <Feather name="x" size={28} color="#EB4D4B" onPress={deleteFunc} /> */}
+        <FontAwesome5 name="pen" size={20} color="#2980B9" onPress={editFunc} />
+        <Feather name="x" size={28} color="#EB4D4B" onPress={deleteFunc} />
       </View>
     </View>
   );
