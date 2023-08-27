a44dbe3fb10b1db73f49d252db9c98e6a99c7273
Ana Laura <analaurass45@gmail.com>
2023-08-27 18:29:03 -0300

    feat: creating modal style

---
+++ frontend/selpe-app/src/components/modal/style.ts
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#E17055',
+    padding: 20,
+    margin: 20,
+    width: '80%',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    borderRadius: 25,
+    elevation: 20,
+    height: 180
+  },
+  title: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    textAlign: 'center'
+  },
+  textTitle: {
+    color: '#FFEAA7',
+    maxWidth: 200,
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  text: {
+    color: '#FFEAA7',
+    textAlign: 'center',
+    justifyContent: 'center'
+  },
+  textContainer: {
+    justifyContent: 'center',
+    height: '90%'
+  }
+});
