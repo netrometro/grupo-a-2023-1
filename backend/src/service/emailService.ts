33df0cf364a6ca489f197b682b1c9138db001a11
Jamuelton <jamueltonangelim@gmail.com>
2023-09-02 12:28:11 -0300

    feat: finishing service to send email

--- backend/src/service/emailService.ts
+++ backend/src/service/emailService.ts
@@ -1,1 +1,24 @@
-const nodeMailer = require("nodemailer");
+import nodeMailer from "nodemailer";
+
+const html = `
+    <h1>Teste de envio de email</h1>
+`;
+
+const transporter = nodeMailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "selpeappmobile@gmail.com",
+    pass: "selpe12345",
+  },
+});
+
+export async function sendEmail(emailData: any) {
+  try {
+    await transporter.sendMail(emailData);
+    console.log("email enviado com sucesso");
+  } catch (error) {
+    console.error("erro ao enviar email", error);
+  }
+}
