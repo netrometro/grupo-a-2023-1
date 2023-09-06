c8841b62bfeb8e43792a9edd8ed38f723e489312
Ana Laura <analaurass45@gmail.com>
2023-09-06 01:05:34 -0300

    fix: deploy

--- backend/src/service/userService.ts
+++ backend/src/service/userService.ts
@@ -94,7 +94,6 @@ export async function addAdress(id: number, endereco: number) {
 }
 
 export async function getUserAdress(id: number) {
-  console.log(prisma.user);
   const user = await prisma.user.findFirst({
     where: { id: Number(id) },
   });
