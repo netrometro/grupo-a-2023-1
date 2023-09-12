16addf42342669284b05cfb56f422f56cda339e0
Ana Laura <analaurass45@gmail.com>
2023-09-12 20:56:50 -0300

    feat: adding provider

--- frontend/selpe-app/App.tsx
+++ frontend/selpe-app/App.tsx
@@ -1,19 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
 import Routes from './src/routes/routes';
+import { AuthProvider } from './src/context/AuthContext';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <AuthProvider>
       <Routes />
-      <StatusBar style="auto" />
-    </View>
+    </AuthProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff'
-  }
-});
