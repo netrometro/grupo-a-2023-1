a925e490860014e6cc9a0db1be4be72860237b63
Jamuelton <jamueltonangelim@gmail.com>
2023-09-12 23:36:17 -0300

    chore: adding Jamuelton's branch code

--- frontend/selpe-app/src/services/Api.ts
+++ frontend/selpe-app/src/services/Api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://192.168.1.4:3000/'
+  baseURL: 'https://selpedeploy-api.onrender.com/'
 });
+
+//http://192.168.1.4:3000/
